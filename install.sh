1691c3eae87bcae8624d98b4d4fc1fe5b4554f07
Axel Xrest <52139213+AxelXrest@users.noreply.github.com>
2024-03-18 10:22:14 +0545

    Finalizing installing packages

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install Yay
-#git clone https://aur.archlinux.org/yay.git
-#cd yay
-#makepkg -si
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 # Install AUR packages
 yay -S --needed - < packageAUR.txt
@@ -12,7 +12,7 @@ yay -S --needed - < packageAUR.txt
 sudo pacman -S --needed - < packagePacman.txt
 
 # Copy configuration files
-#cp -r .config/* ~/.config/
-#cp .oh-my-zsh .bashrc .p10k.zsh .zprofile .zshrc ~/  # Add more as needed
+cp -r .config/* ~/.config/
+cp -r .oh-my-zsh .bashrc .p10k.zsh .zprofile .zshrc ~/  # Add more as needed
 
 echo "Installation completed successfully."
